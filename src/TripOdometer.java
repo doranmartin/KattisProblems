a054a994a0c2fe32f2b134f0b79c626499798312
doranmartin <doranmartin1994@gmail.com>
2023-02-23 16:32:48 -0500

    TripOdometer failed tests due to exceeding time limit

--- src/TripOdometer.java
+++ src/TripOdometer.java
@@ -1,5 +1,6 @@
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.HashSet;
 
 public class TripOdometer {
     public static void main(String[] args) {
@@ -7,15 +8,31 @@ public class TripOdometer {
         // Take in input
         Scanner scanner = new Scanner(System.in);
         int trips = scanner.nextInt();
-        TreeSet<Integer> distances = new TreeSet<>();
+        int[] distancesArray = new int[trips];
         for (int i = 0; i < trips; i++) {
-            distances.add(scanner.nextInt());
+            distancesArray[i] = scanner.nextInt();
         }
 
         // Calculate possibilities
-        int numOfPossibilities = distances.size();
+        HashSet<Integer> distances = new HashSet<>();
+
+        for (int i = 0; i < trips; i++) {
+            int sum = 0;
+            for (int j = 0; j < trips; j++) {
+                if (j != i)
+                    sum += distancesArray[j];
+            }
+            distances.add(sum);
+        }
+        Integer[] possibleTotals = distances.toArray(new Integer[0]);
+        Arrays.sort(possibleTotals);
 
         // Output data
-
+        System.out.println(distances.size());
+        for (int k = 0, m = possibleTotals.length; k < m; k++) {
+            System.out.print(possibleTotals[k]);
+            if (k != m - 1)
+                System.out.print(" ");
+        }
     }
 }
