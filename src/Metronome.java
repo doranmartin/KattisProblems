c31e1b622ec98027d609ededd603863a526269ce
doranmartin <doranmartin1994@gmail.com>
2023-03-11 10:59:23 -0500

    metronome

---
+++ src/Metronome.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Metronome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("%.2f", scanner.nextInt() / 4.0);
+    }
+
+}
