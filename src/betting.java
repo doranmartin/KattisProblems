969fbeb4f7c100d0343df0326929bc5f848bd634
doranmartin <doranmartin1994@gmail.com>
2023-03-12 18:31:52 -0400

    betting

--- src/betting.java
+++ src/betting.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class betting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        System.out.println(100.0 / a);
+        System.out.println(100.0 / (100 - a));
+    }
 }
