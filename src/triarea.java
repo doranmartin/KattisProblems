e4c709feda074d20307d5bb5816ed34f4d0747fa
doranmartin <doranmartin1994@gmail.com>
2023-03-12 19:28:17 -0400

    triarea

---
+++ src/triarea.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class triarea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int h = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(h * b / 2.0);
+    }
+}
