2834222f3fb1cab6301748bccfe35ce726c40b35
doranmartin <doranmartin1994@gmail.com>
2023-03-11 21:30:12 -0500

    cutthenegativity

--- src/cutthenegativity.java
+++ src/cutthenegativity.java
@@ -4,14 +4,21 @@ public class cutthenegativity {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numOfCities = scanner.nextInt();
-        int value;
+        StringBuilder output = new StringBuilder();
+        int numOfDirectFlights = numOfCities * numOfCities;
+        int value = 0;
 
         for (int i = 1; i <= numOfCities; i++) {
             for (int j = 1; j <= numOfCities; j++) {
                 value = scanner.nextInt();
-                if (value != -1)
-                    System.out.printf("");
+                if (value == -1)
+                    numOfDirectFlights--;
+                else
+                    output.append(String.format("%d %d %d\n", i, j, value));
             }
         }
+
+        System.out.println(numOfDirectFlights);
+        System.out.println(output);
     }
 }
