99e38991da153e6b090c026e7b220114df08f2f0
doranmartin <doranmartin1994@gmail.com>
2023-03-13 10:26:34 -0400

    faktor

--- src/faktor.java
+++ src/faktor.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class faktor {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int I = scanner.nextInt();
 
+        System.out.println(A * (I - 1) + 1);
     }
 }
